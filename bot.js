0b0c26cc8db5d1e6518d67d05b0db1004c16d24c
arhidavid <34444292+arhidavid@users.noreply.github.com>
2017-12-11 22:09:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'ping') { 
       var interval = setInterval (function () {
-        message.channel.send("Dear feudalists! Please, note that this channel is for reporting in-game issues only, such as problems with claims, animations, certain actions, characters getting stuck and other issues of type that you experienced during your gameplay. Please, DO NOT post launcher related issues, connectivity issues, login issue or anything that isn't related to the in-game issues here. You can address those questions to https://lifeisfeudal.zendesk.com/hc/en-us And of course, please, DO NOT spam.  Thank you have a good game!")
+        message.channel.send("Dear feudalists! Please, note that this channel is for reporting in-game issues only, such as problems with claims, animations, certain actions, characters getting stuck and other issues of type that you experienced during your gameplay. Please, DO NOT post launcher related issues, connectivity issues, login issue or anything that isn't related to the in-game issues here. You can address those questions to https://lifeisfeudal.zendesk.com/hc/en-us And of course, please, DO NOT spam. Thank you and have a good game!")
       }, 9 * 1000); 
     }
 });
