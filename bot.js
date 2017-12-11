0bb99d725ae11966cc05e57bc2cef09cf42b3cb9
arhidavid <34444292+arhidavid@users.noreply.github.com>
2017-12-11 21:06:28 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,14 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('Have a nice day!');
-  	}
+client.on('message', function() {
+    if (message.content === "$loop") { 
+      var interval = setInterval (function () {
+        bot.sendMessage(message.channel, "Please, note:
+		This channel is for reporting in-game issues, such as problems with claims, animations, certain actions, characters getting stuck and other issues of type that you experienced during your gameplay.
+Please, DO NOT post launcher related issues, connectivity issues, login issue or anything that isn't related to the in-game issues here. Those questons should be addressed to https://lifeisfeudal.zendesk.com/hc/en-us And of course, please, DO NOT spam.  Thank you!")
+      }, 900 * 1000); 
+    }
 });
 
 // THIS  MUST  BE  THIS  WAY
