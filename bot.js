e9b0aebdc94df312eb5ef5abba404011d207694d
arhidavid <34444292+arhidavid@users.noreply.github.com>
2017-12-12 11:50:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,5 +13,11 @@ client.on('message', message => {
     }
 });
 
+client.on('message', message => {
+    if (message.content === 'stop') { 
+      var interval = clearInterval (1); 
+    }
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
